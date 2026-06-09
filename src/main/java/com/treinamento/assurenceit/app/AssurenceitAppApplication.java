d08860c88f9d4bc1bcd32ac08c71d54c14b1b9cb
RonaldoPereiraDM <ronaldo.pereira.dm@gmail.com>
2026-06-09 20:01:09 -0300

    projeto inicial

---
+++ src/main/java/com/treinamento/assurenceit/app/AssurenceitAppApplication.java
@@ -0,0 +1,13 @@
+package com.treinamento.assurenceit.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssurenceitAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssurenceitAppApplication.class, args);
+	}
+
+}
